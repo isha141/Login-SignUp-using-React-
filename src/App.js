38d085896a76cbdccf74b612732e300e94a2fa71
Isha <Ishajindal_20022245,cse@geu.ac.in>
2023-11-16 00:47:20 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginSignUp  from './Components/LoginSignup.jsx';
+function App() {
+  return (
+      <div> 
+      <LoginSignUp/>
+      </div>
+  );
+}
+
+export default App;
